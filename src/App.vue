7f87635227ea823e4013e909e3a958dc1beb99e9
Necritto <onetopyr@mail.ru>
2021-04-18 12:01:47 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nav">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
